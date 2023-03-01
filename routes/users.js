8aa3e0e208095e274551c40140b374beefa71fbf
EZillman <evelina.zillman@medieinstitutet.se>
2023-03-01 22:35:12 +0100

    first endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+let users = [
+  {id: 1, name: 'Choco'},
+  {id: 2, name: 'Hazel'},
+  {id: 3, name: 'Coco'},
+  {id: 4, name: 'Vanilla'}
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(users);
+});
+
+router.get('/test', function(req, res, next) {
+  res.send('test router');
 });
 
 module.exports = router;
